07adf8dabd42d4896f5535cf516e3c7f45531a9a
Rikke <rikkepeterzen@gmail.com>
2023-01-15 15:46:08 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+  .card-type {
+		font-family: 'Arial', sans-serif;
+		background: var(--bg-color);
+        padding: 20px;
+		width: 500px;
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+		grid-gap: 10px;
+		border: #ACC0DE 2px solid;
+        margin: 50px;
+	}
+
+	.card-type img {
+		width: 100%;
+	}
+
+    h3, p {
+        color: var(--hl-color);
+    }
+  </style>
+  <div class="card-type">
+    <img/>
+    <div>
+        <h3></h3>
+        <p><slot name="text" /></p>
+    </div>
+  </div>
+`;
+
+class CardType extends HTMLElement {
+    constructor() {
+        super();
+
+        this.showInfo = true;
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('headline');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('dog');
+    }
+}
+
+window.customElements.define('card-type', CardType);
